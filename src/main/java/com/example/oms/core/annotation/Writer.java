adc249323d6d5eaa81e6df7518ad17c4eb08f291
ryu <jeungwon28@gmail.com>
2026-04-22 15:28:07 +0900

    feat: Reader/Writer custom annotation 추가

---
+++ src/main/java/com/example/oms/core/annotation/Writer.java
@@ -0,0 +1,14 @@
+package com.example.oms.core.annotation;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+@Transactional(propagation = Propagation.MANDATORY)
+public @interface Writer {}
